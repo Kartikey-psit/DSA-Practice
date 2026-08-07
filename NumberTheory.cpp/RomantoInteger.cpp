270d2a9f166794b6de5614f6bf979c45da223e81
Kartikey Singh <mrkartikeyyt07@gmail.com>
2026-08-07 23:44:49 +0530

    Create RomantoInteger.cpp

---
+++ NumberTheory.cpp/RomantoInteger.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+      
+        unordered_map<char, int> romanValues = {
+            {'I', 1}, {'V', 5}, {'X', 10},
+            {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        
+        int result = 0;
+        int n = s.length();
+        
+        for (int i = 0; i < n; ++i) {
+            int currentVal = romanValues[s[i]];
+            int nextVal = (i + 1 < n) ? romanValues[s[i + 1]] : 0;
+            
+            if (currentVal < nextVal) {
+                result -= currentVal;
+            } else {
+                result += currentVal;
+            }
+        }
+        
+        return result;
+    }
+};   
